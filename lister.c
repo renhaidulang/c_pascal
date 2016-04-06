34a98a88e1201ac7a3f570dffc87644ae67cf70e
hechaoyong <he@ubuntu.(none)>
2016-04-06 14:16:35 +0800

    No bugs now.

--- lister.c
+++ lister.c
@@ -6,7 +6,6 @@
 
 static char *source_file_name = NULL;
 static char *date_str = NULL;
-
 static FILE *source_file = NULL;
 
 extern bool lister_init (char *name)
@@ -52,43 +51,43 @@ static void print_page_header (void)
 {
     static int cur_page_num = 0;
 
-    putchar ('\f');
+    printf ("\f\r");
     printf ("Page %d %s %s\n\n", ++cur_page_num, source_file_name, date_str);
 }
 
 static void print_line (char *line)
 {
     const int max_lines_per_page = 50;
+    const int max_print_line_len = 40;
     static int line_cnt = 50;
+    char line_buf[max_print_line_len];
 
     if (++line_cnt > max_lines_per_page) {
         print_page_header ();
         line_cnt = 1;
     }
 
-    printf ("%s", line);
-}
+    int len = 0;
+    char *p = line;
+    len = snprintf (line_buf, sizeof(line_buf), "%s", p);
+    printf ("%s", line_buf);
 
-static void get_line_rest (void)
-{
-    const int read_line_len = 5;
-    char source_buf[read_line_len];
+    while (len >= max_print_line_len && line_buf[max_print_line_len-2] != '\n') {
+        if (++line_cnt > max_lines_per_page) {
+            print_page_header ();
+            line_cnt = 1;
+        }
 
-    if ((fgets (source_buf, read_line_len, source_file)) == NULL) {
-        return ;
-    }
-
-    print_line (source_buf);
-
-    if (strlen (source_buf) == read_line_len-1 &&
-            source_buf[read_line_len-2] != '\n') {
-        get_line_rest ();
+        p += strlen(line_buf);
+        len = snprintf (line_buf, sizeof(line_buf), "%s", p);
+        printf ("\n%s", line_buf);
     }
 }
 
+static bool first = true;
 extern bool get_source_line (void)
 {
-    const int read_line_len = 5;
+    const int read_line_len = 256;
     static int cur_line_num = 0;
 
     char source_buf[read_line_len];
@@ -98,13 +97,20 @@ extern bool get_source_line (void)
         return false;
     }
 
-    cur_line_num++;
-    sprintf (print_buf, "%4d: %s", cur_line_num, source_buf);
+    if (first) {
+        first = false;
+        cur_line_num++;
+        sprintf (print_buf, "%4d: %s", cur_line_num, source_buf);
+    } else {
+        sprintf (print_buf, "%s", source_buf);
+    }
     print_line (print_buf);
 
     if (strlen (source_buf) == read_line_len-1 &&
             source_buf[read_line_len-2] != '\n') {
-        get_line_rest ();
+        get_source_line ();
+    } else {
+        first = true;
     }
 
     return true;
